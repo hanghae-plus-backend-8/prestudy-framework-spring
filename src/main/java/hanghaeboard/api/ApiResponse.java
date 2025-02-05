4e2b6469d3ab1b6b2685ee652bf2ad99b9e71f67
geonyeop123 <64470512+geonyeop123@users.noreply.github.com>
2025-02-05 16:36:19 +0900

    ADD : create api response

---
+++ src/main/java/hanghaeboard/api/ApiResponse.java
@@ -0,0 +1,34 @@
+package hanghaeboard.api;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@NoArgsConstructor
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(int code, HttpStatus status, String message, T data) {
+        this.code = code;
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
+        return new ApiResponse<>(status.value(), status, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return ApiResponse.of(status, status.name(), data);
+    }
+
+    public static <T> ApiResponse<T> ok(T data) {
+        return ApiResponse.of(HttpStatus.OK, data);
+    }
+}
