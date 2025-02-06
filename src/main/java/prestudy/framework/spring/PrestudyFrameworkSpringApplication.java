30b88768f10072ad344276988c0b57b979a00120
discphy <dinophy@nate.com>
2025-02-07 00:02:30 +0900

    feat: 프로젝트 생성

---
+++ src/main/java/prestudy/framework/spring/PrestudyFrameworkSpringApplication.java
@@ -0,0 +1,13 @@
+package prestudy.framework.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestudyFrameworkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestudyFrameworkSpringApplication.class, args);
+	}
+
+}
