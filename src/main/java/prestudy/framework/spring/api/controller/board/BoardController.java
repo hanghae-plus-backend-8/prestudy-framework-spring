06c93b4a7a51d360a269d0aad690e76a7e6ccb82
discphy <dinophy@nate.com>
2025-02-16 22:54:22 +0900

    feat: API Swagger 연동

--- src/main/java/prestudy/framework/spring/api/controller/board/BoardController.java
+++ src/main/java/prestudy/framework/spring/api/controller/board/BoardController.java
@@ -1,5 +1,6 @@
 package prestudy.framework.spring.api.controller.board;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -18,27 +19,32 @@ public class BoardController {
 
     private final BoardService boardService;
 
+    @Tag(name = "게시물 목록 조회")
     @GetMapping("/api/v1/boards")
     public ApiResponse<List<BoardResponse>> getBoards() {
         return ApiResponse.success(boardService.getBoards());
     }
 
+    @Tag(name = "게시물 생성")
     @PostMapping("/api/v1/boards")
     public ApiResponse<BoardResponse> createBoard(@Valid @RequestBody BoardCreateRequest request) {
         return ApiResponse.success(boardService.createBoard(request.toCommand()));
     }
 
+    @Tag(name = "게시물 상세 조회")
     @GetMapping("/api/v1/boards/{id}")
     public ApiResponse<BoardResponse> getBoardById(@PathVariable("id") Long id) {
         return ApiResponse.success(boardService.getBoardById(id));
     }
 
+    @Tag(name = "게시물 수정")
     @PutMapping("/api/v1/boards/{id}")
     public ApiResponse<BoardResponse> updateBoard(@PathVariable("id") Long id,
                                                   @Valid @RequestBody BoardUpdateRequest request) {
         return ApiResponse.success(boardService.updateBoard(request.toCommand(id)));
     }
 
+    @Tag(name = "게시물 삭제")
     @DeleteMapping("/api/v1/boards/{id}")
     public ApiResponse<Void> updateBoard(@PathVariable("id") Long id,
                                          @Valid @RequestBody BoardDeleteRequest request) {
