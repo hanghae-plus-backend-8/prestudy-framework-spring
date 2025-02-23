e313dd4a081580610ff3bb5356f93277964e8cc4
Moon-Jang <roby8502@naver.com>
2025-02-23 22:57:21 +0900

    feat: 게시글 조회

---
+++ src/main/java/com/hhplus/precourse/post/controller/GetPostController.java
@@ -0,0 +1,38 @@
+package com.hhplus.precourse.post.controller;
+
+import com.hhplus.precourse.common.web.ApiResponse;
+import com.hhplus.precourse.post.service.GetPostService;
+import com.hhplus.precourse.post.vo.PostVo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GetPostController {
+    private final GetPostService service;
+
+    @GetMapping("/posts/{id}")
+    ApiResponse<Response> get(@PathVariable Long id) {
+        return ApiResponse.success(
+            Response.from(service.get(id))
+        );
+    }
+
+    record Response(
+        long id,
+        String title,
+        String author,
+        String content
+    ) {
+        public static Response from(PostVo postVo) {
+            return new Response(
+                postVo.id(),
+                postVo.title(),
+                postVo.author(),
+                postVo.content()
+            );
+        }
+    }
+}
---
+++ src/test/java/com/hhplus/precourse/post/controller/GetPostControllerTest.java
@@ -0,0 +1,69 @@
+package com.hhplus.precourse.post.controller;
+
+import com.epages.restdocs.apispec.ResourceSnippetParametersBuilder;
+import com.hhplus.precourse.common.ControllerTestContext;
+import com.hhplus.precourse.post.service.DeletePostService;
+import com.hhplus.precourse.post.service.GetPostService;
+import com.hhplus.precourse.post.vo.PostVo;
+import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+
+import static com.epages.restdocs.apispec.MockMvcRestDocumentationWrapper.document;
+import static com.hhplus.precourse.common.ApiDocumentUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.queryParameters;
+
+class GetPostControllerTest extends ControllerTestContext {
+    private static final String TAG = Tags.POST.tagName();
+    private static final String DESCRIPTION = Tags.POST.descriptionWith("조회");
+
+    @MockBean
+    private GetPostService service;
+
+    @Test
+    void success() {
+        BDDMockito.given(service.get(anyLong()))
+            .willReturn(new PostVo(
+                1L,
+                "작성자명",
+                "제목",
+                "내용",
+                "비밀번호"
+            ));
+
+        given()
+            .when()
+            .get("/posts/{id}", "1")
+            .then()
+            .log().all()
+            .apply(
+                document(
+                    identifier(),
+                    new ResourceSnippetParametersBuilder()
+                        .tag(TAG)
+                        .description(DESCRIPTION),
+                    preprocessRequest(),
+                    preprocessResponse(),
+                    responseFields(
+                        fieldsWithBasic(
+                            fieldWithPath("data").type(OBJECT).description("응답 데이터"),
+                            fieldWithPath("data.id").type(NUMBER).description("게시글 ID"),
+                            fieldWithPath("data.title").type(STRING).description("제목"),
+                            fieldWithPath("data.author").type(STRING).description("작성자명"),
+                            fieldWithPath("data.content").type(STRING).description("내용")
+                        )
+                    )
+                )
+            )
+            .status(HttpStatus.OK);
+    }
+}
