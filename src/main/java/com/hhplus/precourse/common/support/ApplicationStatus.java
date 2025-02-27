e5937317379724bee3f827ba9e9270fbc9648979
Moon-Jang <roby8502@naver.com>
2025-02-27 09:46:39 +0900

    feat: 유저 회원가입

--- src/main/java/com/hhplus/precourse/common/support/ApplicationStatus.java
+++ src/main/java/com/hhplus/precourse/common/support/ApplicationStatus.java
@@ -2,6 +2,12 @@ package com.hhplus.precourse.common.support;
 
 public enum ApplicationStatus implements Status {
     INVALID_PARAMETER("잘못된 요청입니다."),
+
+    // user
+    USER_NOT_FOUND("사용자를 찾을 수 없습니다."),
+    ALREADY_EXIST_USER("이미 존재하는 사용자입니다."),
+
+    // post
     POST_NOT_FOUND("게시글을 찾을 수 없습니다.")
     ;
 
---
+++ src/test/java/com/hhplus/precourse/user/domain/UserTest.java
@@ -0,0 +1,109 @@
+package com.hhplus.precourse.user.domain;
+
+import com.hhplus.precourse.common.exception.DomainException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+class UserTest {
+
+    @Nested
+    @DisplayName("이름 검증 테스트")
+    class ValidateNameTest {
+        @Test
+        void success() {
+            // given
+            var user = new UserFixture()
+                .setName("test1234")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validateName);
+
+            // then
+            assertThat(throwable).isNull();
+        }
+
+        @Test
+        void 이름_길이_검증_실패_케이스() {
+            // given
+            var user = new UserFixture()
+                .setName("11")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validateName);
+
+            // then
+            assertThat(throwable).isInstanceOf(DomainException.class)
+                .hasMessageContaining("이름은 4자 이상 10자 이하로 입력해주세요.");
+        }
+
+        @Test
+        void 이름_패턴_검증_실패_케이스() {
+            // given
+            var user = new UserFixture()
+                .setName("test!@#")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validateName);
+
+            // then
+            assertThat(throwable).isInstanceOf(DomainException.class)
+                .hasMessageContaining("이름은 알파벳 소문자, 숫자로 구성되어야합니다.");
+        }
+    }
+
+    @Nested
+    @DisplayName("비밀번호 검증 테스트")
+    class ValidatePasswordTest {
+        @Test
+        void success() {
+            // given
+            var user = new UserFixture()
+                .setPassword("test1234")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validatePassword);
+
+            // then
+            assertThat(throwable).isNull();
+        }
+
+        @Test
+        void 비밀번호_길이_검증_실패_케이스() {
+            // given
+            var user = new UserFixture()
+                .setPassword("1234567")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validatePassword);
+
+            // then
+            assertThat(throwable).isInstanceOf(DomainException.class)
+                .hasMessageContaining("비밀번호는 8자 이상 15자 이하로 입력해주세요.");
+        }
+
+
+        @Test
+        void 비밀번호_패턴_검증_실패_케이스() {
+            // given
+            var user = new UserFixture()
+                .setPassword("test1234!$")
+                .build();
+
+            // when
+            var throwable = catchThrowable(user::validatePassword);
+
+            // then
+            assertThat(throwable).isInstanceOf(DomainException.class)
+                .hasMessageContaining("비밀번호는 알파벳 대소문자, 숫자로 구성되어야합니다.");
+        }
+    }
+}
